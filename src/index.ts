1f1a6896e1d6ddcb25e477bf8e62cbf0ec759670
hexh <250786313@qq.com>
2022-07-12 16:01:17 +0800

    chore: catalog structure init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Init')
+import { createApp } from './app'
+
+function main() {
+  try {
+    createApp()
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+main()
